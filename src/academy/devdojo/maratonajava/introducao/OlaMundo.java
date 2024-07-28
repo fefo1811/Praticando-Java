7c5c268f9775418be91c23681322e28f43974c56
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-28 14:11:53 -0300

    Iniciando repositório: Aula Tipos Primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/OlaMundo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um JavaDoc
+ */
+// Comentário de uma linha
+public class OlaMundo {
+    public static void main (String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Arrays;
+
+public class TiposPrimitivos {
+    public static void main (String[] args) {
+        int idade = 23;
+        long meuPatrimonio = 100000000;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 128;
+        short idadeShort = 23;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("Minha idade atualmente é " + idade +  " anos.");
+    }
+}
